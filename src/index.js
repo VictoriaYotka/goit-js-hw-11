9f8366fe5f43917ba3a2b8fc8dc3a7d0d35d3f68
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-03-31 19:11:43 +0300

    add load more handler

--- src/index.js
+++ src/index.js
@@ -5,32 +5,52 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formEl  = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more');
+
+let query = '';
+let page = 1;
 
 formEl.addEventListener('submit', formSubmitHandler);
+loadMoreBtnEl.addEventListener('click', loadMoreHandler)
 
 async function formSubmitHandler (event) {
     event.preventDefault();
-    const query = event.target.elements.searchQuery.value.trim();
+    deleteMarkup();
+
+    query = event.target.elements.searchQuery.value.trim();
 
     if(query === '') {
         console.log('empty!')
         return
     }
 
-    const data = await getImages(query);
-    console.log(data);
+    const response = await getImages(query, page);
+    // console.log(response);
+    const data = response.hits;
+    // console.log(data);
 
     if(data.length === 0) {
         Notify.failure("Sorry, there are no images matching your search query. Please try again.")
         return
     }
-    
+
     makeImagesMarkup(data);
+    Notify.info(`Hooray! We found ${response.totalHits} images!`);
+    page += 1;
     
 
 }
 
+async function loadMoreHandler () {
+       const response = await getImages(query, page);
+    // console.log(response);
+    const data = response.hits;
+    // console.log(data);
 
+    makeImagesMarkup(data);
+  
+    page += 1;
+}
 
 function makeImagesMarkup(data) {
     const innerMarkup = data.map(el => `<div class="photo-card">
@@ -53,5 +73,10 @@ function makeImagesMarkup(data) {
    
   galleryEl.insertAdjacentHTML('beforeend', innerMarkup) 
 }
+
+function deleteMarkup () {
+    galleryEl.innerHTML = ''
+}
+
 
   
--- src/js/fetch_function.js
+++ src/js/fetch_function.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-async function getImages(userQuery) {
+async function getImages(userQuery, number) {
     const URL = 'https://pixabay.com/api/';
+    
     const options = {
         params: {
           key: '34916651-f99371deeade2de9e176e6ceb',
@@ -9,15 +10,15 @@ async function getImages(userQuery) {
           image_type: 'photo',
           orientation: 'horizontal',
           safesearch: true,
-    
-    
+          page: number,
+          per_page: 4,
         }
       }
     
         try {
           const response = await axios.get(URL, options);
           // console.log(response.data.hits);
-          return response.data.hits;
+          return response.data;
         } catch (error) {
           console.error(error);
         }
